d34ebfdd6fe44758c2bdd03e00b1f59bfb2938b2
Eugene Rabenko <qwer09846@gmail.com>
2023-11-22 18:39:37 +0200

    add index page in views.py

--- kitchen_restaurant/views.py
+++ kitchen_restaurant/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen_restaurant.models import DishType, Cook, Dish
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_dish_types = DishType.objects.count()
+    num_cooks = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+
+    context = {
+        "num_dish_types": num_dish_types,
+        "num_dishes": num_dishes,
+        "num_cooks": num_cooks
+    }
+
+    return render(request, "kitchen_restaurant/index.html", context=context)
