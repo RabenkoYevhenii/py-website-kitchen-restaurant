fc91ad67251ade1a43f5732ff987d4210b9733b9
Eugene Rabenko <qwer09846@gmail.com>
2023-11-22 12:59:13 +0200

    Initialize empty django project with name py website kitchen restaurant

---
+++ py_website_kitchen_restaurant/asgi.py
@@ -0,0 +1,18 @@
+"""
+ASGI config for py_website_kitchen_restaurant project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "py_website_kitchen_restaurant.settings"
+)
+
+application = get_asgi_application()
