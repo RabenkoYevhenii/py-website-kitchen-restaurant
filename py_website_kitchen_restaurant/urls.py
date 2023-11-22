603b0cf2fcbb2350cc1338eeb8b36b43c33303c5
Eugene Rabenko <qwer09846@gmail.com>
2023-11-22 20:13:58 +0200

    add base.html

--- py_website_kitchen_restaurant/urls.py
+++ py_website_kitchen_restaurant/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from kitchen_restaurant.views import (
+    index
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
+    path("", index, name="index")
 ]
