2a45a2d62a74cf7633ec1090e6af7eae0ad880d0
Eugene Rabenko <qwer09846@gmail.com>
2023-11-22 19:42:10 +0200

    add djdt to project

--- py_website_kitchen_restaurant/settings.py
+++ py_website_kitchen_restaurant/settings.py
@@ -29,6 +29,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 # Application definition
 
@@ -39,13 +42,15 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "kitchen_restaurant",
+    "debug_toolbar",
     "crispy_forms",
     "crispy_bootstrap5",
+    "kitchen_restaurant",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
