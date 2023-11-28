09cb511dd9f30378477c7ac7a0ee48d7b6d7a41f
Eugene Rabenko <qwer09846@gmail.com>
2023-11-28 18:35:02 +0200

    delete unsed import

--- kitchen_restaurant/forms.py
+++ kitchen_restaurant/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-from django.utils import timezone
 
 from kitchen_restaurant.models import Dish, Cook
 
